8b695c9797408871f652db9858ea201b65bd16ed
Pooja <poojaprakasan00@gmail.com>
2026-09-18 17:43:28 +0530

    feat(models) : Register new models in models package

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,3 +1,13 @@
+from app.models.app_usage import AppUsage
+from app.models.browser_activity import BrowserActivity
+from app.models.permission import Permission
 from app.models.user import User
+from app.models.youtube_activity import YouTubeActivity
 
-__all__ = ["User"]
+__all__ = [
+    "User",
+    "Permission",
+    "AppUsage",
+    "BrowserActivity",
+    "YouTubeActivity",
+]
