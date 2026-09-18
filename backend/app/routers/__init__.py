61f421ac39b713b4f0e65c97f6e556cded1d7f41
Pooja <poojaprakasan00@gmail.com>
2026-09-18 17:56:38 +0530

    feat(routers) : Register tracking router in routers package

--- backend/app/routers/__init__.py
+++ backend/app/routers/__init__.py
@@ -1,3 +1,4 @@
 from app.routers.auth import router as auth_router
+from app.routers.tracking import router as tracking_router
 
-__all__ = ["auth_router"]
+__all__ = ["auth_router", "tracking_router"]
